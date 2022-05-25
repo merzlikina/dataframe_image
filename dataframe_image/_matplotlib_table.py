aacec43df72b12b77fab49e239955f767bacd5d6
merzlikina <merzlikina.p.m@gmail.com>
2022-05-25 18:36:51 +0300

    Add background matplotlib table color+autopep

--- dataframe_image/_matplotlib_table.py
+++ dataframe_image/_matplotlib_table.py
@@ -12,8 +12,8 @@ from matplotlib.backends.backend_agg import RendererAgg
 
 
 class TableMaker:
-    
-    def __init__(self, fontsize=14, encode_base64=True, limit_crop=True, for_document=True):
+
+    def __init__(self, fontsize=14, encode_base64=True, limit_crop=True, for_document=True, background_color="#f5f5f5"):
         self.original_fontsize = fontsize
         self.encode_base64 = encode_base64
         self.limit_crop = limit_crop
@@ -26,7 +26,8 @@ class TableMaker:
             self.figheight = 4
             self.wrap_length = 10
         self.dpi = 100
-        
+        self.background_color = background_color
+
     def parse_html(self, html):
         html = html.replace('<br>', '\n')
         rows, num_header_rows = self.parse_into_rows(html)
@@ -50,7 +51,7 @@ class TableMaker:
                     col_loc += cur_col_loc
                 else:
                     val = row[j]
-                    if val[-2] > 1: # new rowspan detected
+                    if val[-2] > 1:  # new rowspan detected
                         rowspan[col_loc] = val
                     col_loc += val[-1]  # usually 1
                     for _ in range(val[-1]):
@@ -58,7 +59,7 @@ class TableMaker:
                     j += 1
             new_rows.append(new_row)
         return new_rows, num_header_rows
-    
+
     def get_text_align(self, element):
         style = element.get('style', '').lower()
         if 'text-align' in style:
@@ -67,7 +68,7 @@ class TableMaker:
             for val in ('left', 'right', 'center'):
                 if text_align.startswith(val):
                     return val
-    
+
     def parse_into_rows(self, html):
         def parse_row(row):
             values = []
@@ -85,10 +86,11 @@ class TableMaker:
 
         soup = BeautifulSoup(html, features="lxml")
         # get number of columns from first row
-        num_cols = sum(int(el.get('colspan', 1)) for el in soup.find('tr').find_all(['td', 'th']))
+        num_cols = sum(int(el.get('colspan', 1))
+                       for el in soup.find('tr').find_all(['td', 'th']))
         thead = soup.find('thead')
         tbody = soup.find('tbody')
-        
+
         rows = []
         if thead:
             head_rows = thead.find_all('tr')
@@ -97,24 +99,24 @@ class TableMaker:
                     rows.append(parse_row(row))
             else:
                 rows.append(parse_row(thead))
-                
+
         num_header_rows = len(rows)
-        
+
         if tbody:
             for row in tbody.find_all('tr'):
                 rows.append(parse_row(row))
-                
+
         if not thead and not tbody:
             for row in soup.find_all('tr'):
                 rows.append(parse_row(row))
         return rows, num_header_rows
-    
+
     def get_text_width(self, text):
         fig = self.text_fig
         t = fig.text(0, 0, text, size=self.fontsize)
         bbox = t.get_window_extent(renderer=self.renderer)
         return bbox.width
-    
+
     def get_all_text_widths(self, rows):
         all_text_widths = []
         for row in rows:
@@ -122,15 +124,16 @@ class TableMaker:
             for vals in row:
                 cell_max_width = 0
                 for text in vals[0].split('\n'):
-                    cell_max_width = max(cell_max_width, self.get_text_width(text))
+                    cell_max_width = max(
+                        cell_max_width, self.get_text_width(text))
                 row_widths.append(cell_max_width)
             all_text_widths.append(row_widths)
-        pad = 10 # number of pixels to pad columns with
+        pad = 10  # number of pixels to pad columns with
         return np.array(all_text_widths) + 15
-    
+
     def calculate_col_widths(self):
         all_text_widths = self.get_all_text_widths(self.rows)
-        max_col_widths = all_text_widths.max(axis=0)        
+        max_col_widths = all_text_widths.max(axis=0)
         mult = 1
         total_width = self.figwidth * self.dpi
         if self.for_document and sum(max_col_widths) >= total_width:
@@ -143,14 +146,14 @@ class TableMaker:
                     max_col_widths = all_text_widths.max(axis=0)
                     if sum(max_col_widths) < total_width:
                         break
-                    
+
             if mult <= .5 and self.fontsize > 12:
                 self.fontsize *= .9
                 return self.calculate_col_widths()
             else:
                 self.rows = rows
                 total_width = sum(max_col_widths)
-            
+
         col_prop = [width / total_width for width in max_col_widths]
         return col_prop
 
@@ -165,7 +168,8 @@ class TableMaker:
                 width = max(self.wrap_length, int(len(text) * mult))
                 new_text = textwrap.fill(text, width, break_long_words=False)
                 new_texts.append(new_text)
-                new_max_width = max(new_max_width, self.get_text_width(new_text))
+                new_max_width = max(
+                    new_max_width, self.get_text_width(new_text))
             else:
                 new_texts.append(text)
 
@@ -173,33 +177,32 @@ class TableMaker:
             for row, text in zip(rows, new_texts):
                 row[idx][0] = text
         return rows
-    
+
     def get_row_heights(self):
         row_heights = []
         for row in self.rows:
             row_count = max([val[0].count('\n') + 1 for val in row])
             height = (row_count + 1) * self.fontsize / 72
             row_heights.append(height)
-            
+
         return row_heights
-    
+
     def create_figure(self):
         figheight = sum(self.row_heights)
         fig = Figure(dpi=self.dpi, figsize=(self.figwidth, figheight))
         return fig
-        
+
     def print_table(self):
-        row_colors = ["#f5f5f5", "#ffffff"]
         total_width = sum(self.col_widths)
         figheight = self.fig.get_figheight()
         row_locs = [height / figheight for height in self.row_heights]
-            
+
         header_text_align = [vals[2] for vals in self.rows[0]]
         x0 = (1 - total_width) / 2
         x = x0
         yd = row_locs[0]
         y = 1
-            
+
         for i, (yd, row) in enumerate(zip(row_locs, self.rows)):
             x = x0
             y -= yd
@@ -212,23 +215,22 @@ class TableMaker:
                     x += xd
                 elif ha == 'center':
                     x += xd / 2
-                self.fig.text(x, y + yd / 2, text, family='Helvetica', 
+                self.fig.text(x, y + yd / 2, text, family='Helvetica',
                               size=self.fontsize, ha=ha, va='center', weight=weight)
                 if ha == 'left':
                     x += xd
                 elif ha == 'center':
                     x += xd / 2
-                
-            diff = i - self.num_header_rows
-            if diff >= 0 and  diff % 2 == 0:
-                p = mpatches.Rectangle((x0, y), width=total_width, height=yd, fill=True, 
-                                       color='#f5f5f5', transform=self.fig.transFigure)
-                self.fig.add_artist(p)  
-                
+
+            p = mpatches.Rectangle((x0, y), width=total_width, height=yd, fill=True,
+                                   color=self.background_color, transform=self.fig.transFigure)
+            self.fig.add_artist(p)
+
             if i == self.num_header_rows - 1:
-                line = mlines.Line2D([x0, x0 + total_width], [y, y], color='black')
+                line = mlines.Line2D(
+                    [x0, x0 + total_width], [y, y], color='black')
                 self.fig.add_artist(line)
-                
+
         w, h = self.fig.get_size_inches()
         start = self.figwidth * min(x0, .1)
         end = self.figwidth - start
--- dataframe_image/_pandas_accessor.py
+++ dataframe_image/_pandas_accessor.py
@@ -6,6 +6,8 @@ from ._screenshot import Screenshot
 MAX_COLS = 30
 MAX_ROWS = 100
 
+BACKGROUND_COLOR_DEFAULT = "#f5f5f5"
+
 
 @pd.api.extensions.register_dataframe_accessor("dfi")
 class _Export:
@@ -13,18 +15,18 @@ class _Export:
     def __init__(self, df):
         self._df = df
 
-    def export(self, filename, fontsize=14, max_rows=None, max_cols=None, 
+    def export(self, filename, fontsize=14, max_rows=None, max_cols=None,
                table_conversion='chrome', chrome_path=None):
-        return _export(self._df, filename, fontsize, max_rows, max_cols, 
+        return _export(self._df, filename, fontsize, max_rows, max_cols,
                        table_conversion, chrome_path)
 
 
-def export(obj, filename, fontsize=14, max_rows=None, max_cols=None, 
-               table_conversion='chrome', chrome_path=None):
-        return _export(obj, filename, fontsize, max_rows, max_cols, table_conversion, chrome_path)
-        
+def export(obj, filename, fontsize=14, max_rows=None, max_cols=None,
+           table_conversion='chrome', chrome_path=None, background_color=BACKGROUND_COLOR_DEFAULT):
+    return _export(obj, filename, fontsize, max_rows, max_cols, table_conversion, chrome_path, background_color)
 
-def _export(obj, filename, fontsize, max_rows, max_cols, table_conversion, chrome_path):    
+
+def _export(obj, filename, fontsize, max_rows, max_cols, table_conversion, chrome_path, background_color=BACKGROUND_COLOR_DEFAULT):
     is_styler = isinstance(obj, Styler)
     df = obj.data if is_styler else obj
 
@@ -33,33 +35,34 @@ def _export(obj, filename, fontsize, max_rows, max_cols, table_conversion, chrom
                                fontsize=fontsize, encode_base64=False, limit_crop=False).run
     else:
         from ._matplotlib_table import TableMaker
-        converter = TableMaker(fontsize=fontsize, encode_base64=False, for_document=False).run
+        converter = TableMaker(fontsize=fontsize, encode_base64=False,
+                               for_document=False, background_color=background_color).run
 
     if df.shape[0] > MAX_ROWS and max_rows is None:
         error_msg = (f'Your DataFrame has more than {MAX_ROWS} rows and will produce a huge '
-                    'image file, possibly causing your computer to crash. Override this error '
-                    'by explicitly setting `max_rows`. Use -1 for all rows.')
+                     'image file, possibly causing your computer to crash. Override this error '
+                     'by explicitly setting `max_rows`. Use -1 for all rows.')
         if is_styler:
             error_msg = (f'Your Styled DataFrame has more than {MAX_ROWS} rows and will produce '
-                        'a huge image file, possibly causing your computer to crash. Override '
-                        'this error by explicitly setting `max_rows` to -1 for all columns. '
-                        'Styled DataFrames are unable to select a subset of rows or columns '
-                        'and therefore do not work with the `max_rows` and `max_cols` parameters')
+                         'a huge image file, possibly causing your computer to crash. Override '
+                         'this error by explicitly setting `max_rows` to -1 for all columns. '
+                         'Styled DataFrames are unable to select a subset of rows or columns '
+                         'and therefore do not work with the `max_rows` and `max_cols` parameters')
         raise ValueError(error_msg)
     if max_rows == -1:
         max_rows = None
 
     if df.shape[1] > MAX_COLS and max_cols is None:
         error_msg = (f'Your DataFrame has more than {MAX_COLS} columns and will produce a huge '
-                    'image file, possibly causing your computer to crash. Override this error '
-                    'by explicitly setting `max_cols`. Use -1 for all columns.')
+                     'image file, possibly causing your computer to crash. Override this error '
+                     'by explicitly setting `max_cols`. Use -1 for all columns.')
         if is_styler:
             error_msg = (f'Your Styled DataFrame has more than {MAX_COLS} columns and will '
-                        'produce a huge image file, possibly causing your computer to crash. '
-                        'Override this error by explicitly setting `max_cols` to -1 for '
-                        'all columns. Styled DataFrames are unable to select a subset of '
-                        'rows or columns and therefore do not work with the `max_rows` '
-                        'and `max_cols` parameters')
+                         'produce a huge image file, possibly causing your computer to crash. '
+                         'Override this error by explicitly setting `max_cols` to -1 for '
+                         'all columns. Styled DataFrames are unable to select a subset of '
+                         'rows or columns and therefore do not work with the `max_rows` '
+                         'and `max_cols` parameters')
         raise ValueError(error_msg)
 
     if max_cols == -1:
@@ -69,13 +72,14 @@ def _export(obj, filename, fontsize, max_rows, max_cols, table_conversion, chrom
         html = '<div>' + obj.render() + '</div>'
     else:
         html = obj.to_html(max_rows=max_rows, max_cols=max_cols, notebook=True)
-    
+
     img_str = converter(html)
 
     if isinstance(filename, str):
         open(filename, 'wb').write(img_str)
     elif hasattr(filename, 'write'):
         filename.write(img_str)
+
 
 setattr(Styler, 'export_png', export)
 
@@ -91,7 +95,7 @@ Export a styled DataFrame as png to a file
 Parameters
 ----------"""
 
-doc_params =f"""
+doc_params = f"""
 filename : str or file-like
     The file location where the image will be saved. Provide a string 
     to specify a file location on your local machine or a file-like object 
@@ -127,7 +131,7 @@ chrome_path : str, default `None`
 
 """
 
-export_intro ="""
+export_intro = """
 Export a DataFrame or Styled DataFrame as a png to a file.
 
 Styled DataFrames cannot use max_rows or max_cols and 
